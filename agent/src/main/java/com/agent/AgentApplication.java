aad728a9adde0c2cea52d317096769760d1324ea
Geonbeom Kim <gunibumi23@gmail.com>
2024-02-01 13:49:17 +0900

    커밋

--- agent/src/main/java/com/agent/AgentApplication.java
+++ agent/src/main/java/com/agent/AgentApplication.java
@@ -12,6 +12,7 @@ import lombok.extern.slf4j.Slf4j;
 public class AgentApplication {
 	
 	public static void main(String[] args) {
+		
 		AgentDbInitailize agentIni = new AgentDbInitailize("agent");
 		agentIni.build();		
 		SpringApplication.run(AgentApplication.class, args);		
